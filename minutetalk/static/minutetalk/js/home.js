c21f2fc26b530d03d35f7abb2bbb2914c61a1b6c
mfcobo <mlfcobo@gmail.com>
2018-07-18 15:31:36 +0800

    Improve livestream UI

--- minutetalk/static/minutetalk/js/home.js
+++ minutetalk/static/minutetalk/js/home.js
@@ -25,6 +25,7 @@ var vue = new Vue({
             gender: ['All', 'Male', 'Female'],
             agechoice: 'All',
             age: ['All', '18-24', '25-34', '35-44', '45-54', '55-64', '65+'],
+            notClicked: true,
             form: {
                 fname: '',
                 lname: '',
